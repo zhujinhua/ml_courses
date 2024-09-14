9fb9125aa19d9430575a745ceac6f2ff7fb7c31f
jhzhu <jhzhu@telenavsoftware.com>
2024-09-14 10:06:33 +0800

    exam

--- deep_learning/exam/deep_learning_01.py
+++ deep_learning/exam/deep_learning_01.py
@@ -4,13 +4,14 @@
 import torch
 from torch import nn
 
+
 class Model(nn.Module):
 
     def __init__(self, n_features=13):
         super(Model, self).__init__()
         self.linear1 = nn.Linear(in_features=n_features, out_features=8)
         self.linear2 = nn.Linear(in_features=8, out_features=1)
-    
+
     def forward(self, x):
         x = self.linear1(x)
         x = torch.relu(x)
--- deep_learning/exam/deep_learning_02.py
+++ deep_learning/exam/deep_learning_02.py
@@ -4,37 +4,38 @@
 import torch
 from torch import nn
 
+
 class Model(nn.Module):
 
     def __init__(self):
         super(Model, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=1, 
+        self.conv1 = nn.Conv2d(in_channels=1,
                                out_channels=16,
                                kernel_size=3,
                                stride=1,
                                padding=1)
         self.bn1 = nn.BatchNorm2d(num_features=16)
-        self.mp1 = nn.MaxPool2d(kernel_size=2, 
-                                stride=2, 
+        self.mp1 = nn.MaxPool2d(kernel_size=2,
+                                stride=2,
                                 padding=0)
-        self.conv2 = nn.Conv2d(in_channels=16, 
+        self.conv2 = nn.Conv2d(in_channels=16,
                                out_channels=64,
                                kernel_size=3,
                                stride=1,
                                padding=1)
         self.bn2 = nn.BatchNorm2d(num_features=64)
-        self.mp2 = nn.MaxPool2d(kernel_size=2, 
-                                        stride=2, 
-                                        padding=0)
+        self.mp2 = nn.MaxPool2d(kernel_size=2,
+                                stride=2,
+                                padding=0)
         self.linear1 = nn.Linear(in_features=64 * 7 * 7, out_features=512)
         self.linear2 = nn.Linear(in_features=512, out_features=10)
-    
+
     def forward(self, x):
         x = self.conv1(x)
         x = self.bn1(x)
         x = torch.relu(x)
         x = self.mp1(x)
-        
+
         x = self.conv2(x)
         x = self.bn2(x)
         x = torch.relu(x)
